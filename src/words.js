0fc00e73d85a7480125eff3a00caaf5b245554e3
Manish Ambre <manishambre5@gmail.com>
2025-05-05 12:23:38 +0200

    vocabulary append

--- src/words.js
+++ src/words.js
@@ -1,98 +1,266 @@
 export const triads = [
-    {
-      "connection": "Chris ___",
-      "words": ["Evans","Hemsworth","Pratt"]
-    },
-    {
-      "connection": "Pizza Toppings",
-      "words": ["Pepperoni","Mushrooms","Pineapple"]
-    },
-    {
-      "connection": "Jupiter's Moons",
-      "words": ["Ganymede","Europa","Io"]
-    },
-    {
-      "connection": "Shades of Blue",
-      "words": ["Navy","Sky","Indigo"]
-    },
-    {
-      "connection": "Big Cats",
-      "words": ["Tiger","Jaguar","Puma"]
-    },
-    {
-      "connection": "Cheese",
-      "words": ["Cheddar","Feta","Ricotta"]
-    },
-    {
-      "connection": "Fungi",
-      "words": ["Yeast","Mushrooms","Mold"]
-    },
-    {
-      "connection": "Fermented beverages",
-      "words": ["Kvass","Kefir","Boza"]
-    },
-    {
-      "connection": "Poem forms",
-      "words": ["Haiku","Sonnet","Limerick"]
-    },
-    {
-      "connection": "Musical Instruments",
-      "words": ["Ukulele","Mandolin","Banjo"]
-    },
-    {
-      "connection": "Kitchen tools",
-      "words": ["Whisk","Tongs","Mandoline"]
-    },
-    {
-      "connection": "Financial Documents",
-      "words": ["Ledger","Invoice","Receipt"]
-    },
-    {
-      "connection": "King",
-      "words": ["Pharaoh","Sultan","Maharaja"]
-    },
-    {
-      "connection": "Ancient Supercontinents",
-      "words": ["Pangea","Gondwana","Tethys"]
-    },
-    {
-      "connection": "River Systems of Asia",
-      "words": ["Yangtze","Indus","Mekong"]
-    },
-    {
-      "connection": "Mountains",
-      "words": ["Kilimanjaro","Matterhorn","Annapurna"]
-    },
-    {
-      "connection": "Mountain Ranges",
-      "words": ["Andes","Rocky","Alps"]
-    },
-    {
-      "connection": "African Languages",
-      "words": ["Swahili","Hausa","Xhosa"]
-    },
-    {
-      "connection": "Made of Carbon",
-      "words": ["Glucose","Ethanol","Diamond"]
-    },
-    {
-      "connection": "Saturn's Moons",
-      "words": ["Rhea","Titan","Mimas"]
-    },
-    {
-      "connection": "Linux Distributions",
-      "words": ["Ubuntu","Debian","Fedora"]
-    },
-    {
-      "connection": "Herbs",
-      "words": ["Thyme","Basil","Rosemary"]
-    },
-    {
-      "connection": "Flatbreads",
-      "words": ["Naan","Tortilla","Pita"]
-    },
-    {
-      "connection": "Revolutionaries",
-      "words": ["Che","Thomas","Rosa"]
-    },
+  {
+    "connection": "Chris ___",
+    "words": ["Evans","Hemsworth","Pratt"]
+  },
+  {
+    "connection": "Pizza Toppings",
+    "words": ["Pepperoni","Mushrooms","Pineapple"]
+  },
+  {
+    "connection": "Jupiter's Moons",
+    "words": ["Ganymede","Europa","Io"]
+  },
+  {
+    "connection": "Shades of Blue",
+    "words": ["Navy","Sky","Indigo"]
+  },
+  {
+    "connection": "Big Cats",
+    "words": ["Tiger","Jaguar","Puma"]
+  },
+  {
+    "connection": "Cheese",
+    "words": ["Cheddar","Feta","Ricotta"]
+  },
+  {
+    "connection": "Fungi",
+    "words": ["Yeast","Mushrooms","Mold"]
+  },
+  {
+    "connection": "Fermented beverages",
+    "words": ["Kvass","Kefir","Boza"]
+  },
+  {
+    "connection": "Poem forms",
+    "words": ["Haiku","Sonnet","Limerick"]
+  },
+  {
+    "connection": "Musical Instruments",
+    "words": ["Ukulele","Mandolin","Banjo"]
+  },
+  {
+    "connection": "Kitchen tools",
+    "words": ["Whisk","Tongs","Mandoline"]
+  },
+  {
+    "connection": "Financial Documents",
+    "words": ["Ledger","Invoice","Receipt"]
+  },
+  {
+    "connection": "King",
+    "words": ["Pharaoh","Sultan","Maharaja"]
+  },
+  {
+    "connection": "Ancient Supercontinents",
+    "words": ["Pangea","Gondwana","Tethys"]
+  },
+  {
+    "connection": "River Systems of Asia",
+    "words": ["Yangtze","Indus","Mekong"]
+  },
+  {
+    "connection": "Mountains",
+    "words": ["Kilimanjaro","Matterhorn","Annapurna"]
+  },
+  {
+    "connection": "Mountain Ranges",
+    "words": ["Andes","Rocky","Alps"]
+  },
+  {
+    "connection": "African Languages",
+    "words": ["Swahili","Hausa","Xhosa"]
+  },
+  {
+    "connection": "Made of Carbon",
+    "words": ["Glucose","Ethanol","Diamond"]
+  },
+  {
+    "connection": "Saturn's Moons",
+    "words": ["Rhea","Titan","Mimas"]
+  },
+  {
+    "connection": "Linux Distributions",
+    "words": ["Ubuntu","Debian","Fedora"]
+  },
+  {
+    "connection": "Herbs",
+    "words": ["Thyme","Basil","Rosemary"]
+  },
+  {
+    "connection": "Flatbreads",
+    "words": ["Naan","Tortilla","Pita"]
+  },
+  {
+    "connection": "Revolutionaries",
+    "words": ["Che","Thomas","Rosa"]
+  },
+  {
+    "connection": "Catch a ___",
+    "words": ["Breath","Break","Glimpse"]
+  },
+  {
+    "connection": "Take the ___",
+    "words": ["Bait","Cake","Beating"]
+  },
+  {
+    "connection": "Hit the ___",
+    "words": ["Sack","Road","Roof"]
+  },
+  {
+    "connection": "Off the ___",
+    "words": ["Charts","Cuff","Hook"]
+  },
+  {
+    "connection": "Last night was ___",
+    "words": ["Wild","Crazy","Insane"]
+  },
+  {
+    "connection": "A piece of ___",
+    "words": ["Cake","Work","Me"]
+  },
+  {
+    "connection": "God of Thunder",
+    "words": ["Thor","Zeus","Indra"]
+  },
+  {
+    "connection": "Ancient Geographical Regions",
+    "words": ["Persia","Nubia","Mesopotamia"]
+  },
+  {
+    "connection": "Types of Bears",
+    "words": ["Grizzly","Panda","Polar"]
+  },
+  {
+    "connection": "Musketeers",
+    "words": ["Athos","Porthos","Aramis"]
+  },
+  {
+    "connection": "The Beatles",
+    "words": ["Paul","John","George"]
+  },
+  {
+    "connection": "Programming Languages",
+    "words": ["Cobalt","Python","Ruby"]
+  },
+  {
+    "connection": "World Currencies",
+    "words": ["Yuan","Yen","Sterling"]
+  },
+  {
+    "connection": "Dog Breeds",
+    "words": ["Beagle","Poodle","Labrador"]
+  },
+  {
+    "connection": "Bones in the Body",
+    "words": ["Femur", "Skull", "Rib"]
+  },
+  {
+    "connection": "Fictional Detectives",
+    "words": ["Sherlock", "Poirot", "Marple"]
+  },
+  {
+    "connection": "Types of Poems",
+    "words": ["Ode", "Elegy", "Ballad"]
+  },
+  {
+    "connection": "Romance Languages",
+    "words": ["Spanish", "French", "Romanian"]
+  },
+  {
+    "connection": "Latin American Countries",
+    "words": ["Cuba", "Chile", "Peru"]
+  },
+  {
+    "connection": "Insects",
+    "words": ["Ant", "Bee", "Wasp"]
+  },
+  {
+    "connection": "Trees",
+    "words": ["Oak", "Pine", "Maple"]
+  },
+  {
+    "connection": "Types of Pasta",
+    "words": ["Spaghetti", "Fusilli", "Penne"]
+  },
+  {
+    "connection": "Elements from the Periodic Table",
+    "words": ["Neon", "Argon", "Carbon"]
+  },
+  {
+    "connection": "Birds",
+    "words": ["Flamingo", "Sparrow", "Penguin"]
+  },
+  {
+    "connection": "Scientists",
+    "words": ["Newton", "Einstein", "Curie"]
+  },
+  {
+    "connection": "Astronomers",
+    "words": ["Kepler", "Galileo", "Copernicus"]
+  },
+  {
+    "connection": "Mathematicians",
+    "words": ["Aryabhatta", "Turing", "Archimedes"]
+  },
+  {
+    "connection": "Biologists",
+    "words": ["Darwin", "Fleming", "Mendel"]
+  },
+  {
+    "connection": "Engineers",
+    "words": ["Tesla", "Wright", "da Vinci"]
+  },
+  {
+    "connection": "Video Game Characters",
+    "words": ["Mario", "Kratos", "Sonic"]
+  },
+  {
+    "connection": "Synonyms for 'Fast'",
+    "words": ["Quick", "Rapid", "Speedy"]
+  },
+  {
+    "connection": "Slang for 'Money'",
+    "words": ["Dough", "Bread", "Cheddar"]
+  },
+  {
+    "connection": "Hang ___",
+    "words": ["Out", "Tight", "On"]
+  },
+  {
+    "connection": "Very Tired",
+    "words": ["Exhausted", "Wiped", "Drained"]
+  },
+  {
+    "connection": "In a ___",
+    "words": ["Bind", "Jam", "Pickle"]
+  },
+  {
+    "connection": "Leave Quickly",
+    "words": ["Bounce", "Dip", "Scram"]
+  },
+  {
+    "connection": "Happy",
+    "words": ["Joyous", "Elated", "Ecstatic"]
+  },
+  {
+    "connection": "Cool",
+    "words": ["Dope", "Lit", "Sick"]
+  },
+  {
+    "connection": "Angry",
+    "words": ["Pissed", "Livid", "Steamed"]
+  },
+  {
+    "connection": "A Friend",
+    "words": ["Buddy", "Pal", "Homie"]
+  },
+  {
+    "connection": "Drop the ___",
+    "words": ["Ball", "Mic", "Act"]
+  },
+  {
+    "connection": "Under the ___",
+    "words": ["Weather", "Radar", "Table"]
+  },
 ];
